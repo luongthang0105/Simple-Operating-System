4cd920ee1e0f26f25b116a16b46626014c17a45b
Han <z5452011@ad.unsw.edu.au>
2025-10-06 15:36:59 +1100

    feat: impl sos_usleep and sos_time

--- projects/aos/libsosapi/src/sos.c
+++ projects/aos/libsosapi/src/sos.c
@@ -156,13 +156,19 @@ pid_t sos_process_wait(pid_t pid)
 
 }
 
-void sos_usleep(int msec)
+void sos_usleep(int usec)
 {
-    assert(!"You need to implement this");
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 2);
+    seL4_SetMR(0, SYSCALL_SOS_USLEEP);
+    seL4_SetMR(1, usec);
+    seL4_Call(SOS_IPC_EP_CAP, tag);
 }
 
 int64_t sos_time_stamp(void)
 {
-    assert(!"You need to implement this");
-    return -1;
+    seL4_MessageInfo_t tag = seL4_MessageInfo_new(0, 0, 0, 1);
+    seL4_SetMR(0, SYSCALL_SOS_TIMESTAMP); 
+    seL4_Call(SOS_IPC_EP_CAP, tag);
+    int64_t timestamp = seL4_GetMR(0);
+    return timestamp;
 }
