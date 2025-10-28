6a758dbefd4b7a2efe126a69685285f4a622af09
Han <z5452011@ad.unsw.edu.au>
2025-10-28 16:20:03 +1100

    feat: write some test for open function

--- projects/aos/apps/tests/src/file_system/file_system.c
+++ projects/aos/apps/tests/src/file_system/file_system.c
@@ -14,37 +14,58 @@
 
 #define BUF_SIZ    6144
 #define MAX_ARGS   32
+static int fd;
+static sos_stat_t sbuf;
 
 //========================== OPEN ==========================
-
 int test_open_console() {
-    int in = open("console", O_RDONLY);
-    assert(in == CONSOLE_FD);
-
-    printf("✅ test_open_console\n");
+    fd = open("console", O_RDONLY);
+    assert(fd == CONSOLE_FD);
 }
 
 int test_open_non_existent_file() {
-    sos_stat_t sbuf;
-
-    int fd = open("new_file.txt", O_RDONLY);
+    fd = open("new_file.txt", O_RDONLY);
     assert(fd > 0);
 
     int res = sos_stat("new_file.txt", &sbuf);
     assert(res == 0);
 
     assert(sbuf.st_type == ST_FILE);
-    // Non-existent 
+
+    // Non-existent file should have read-write permission
     assert((sbuf.st_fmode & FM_READ) != 0);
     assert((sbuf.st_fmode & FM_WRITE) != 0);
     assert((sbuf.st_fmode & FM_EXEC) == 0);
+}
 
-    printf("✅ test_open_console\n");
+int test_read_file_opened_with_read_mode() {
+    fd = open("file.txt", O_RDONLY);
+    char buf[BUF_SIZ];
+    int num_read = read(fd, buf, BUF_SIZ);
+    assert(num_read == 0);
+}
+
+int test_read_file_opened_with_write_mode() {
+    fd = open("file.txt", O_WRONLY);
+    char buf[BUF_SIZ];
+    int res = read(fd, buf, BUF_SIZ);
+    // assert(res == -1);
 }
 
 int test_file_system() {
+    // open files
     test_open_console();
     test_open_non_existent_file();
+
+    // read from file
+    test_read_file_opened_with_read_mode();
+    test_read_file_opened_with_write_mode();
+
+    // write to file
+
+    // file stat
+    
+    printf("File system test\tPassed\n");
 }
 
 
